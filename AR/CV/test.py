406bea9d7eda3c3eda560a42919369695fc4e0c5
thedoctormarc <36292102+thedoctormarc@users.noreply.github.com>
2020-02-12 12:11:49 +0100

    Pushing last class

---
+++ AR/CV/test.py
@@ -0,0 +1,20 @@
+print("Python is a slow language")
+
+number = input("Text number: ")
+print(number)
+
+a = False
+
+# while a:
+#     print("Running")
+
+def func(x): 
+    myList = {x, x, x}
+    print(myList)
+
+class Weapon:
+    def __init__(self, name): 
+        self.name = name
+        func(666)
+
+bullet = Weapon("AK-47 " * 3)
